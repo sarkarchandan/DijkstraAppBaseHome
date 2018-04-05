610abcddbb67b9383678c2e5791e1895d523986c
chandan <elitegreen@gmail.com>
2018-04-05 19:39:57 +0200

    [EH] Initial Commit

---
+++ DijkstraAppSharedTest/DijkstraAppSharedTest.swift
@@ -0,0 +1,16 @@
+//
+//  DijkstraAppSharedTest.swift
+//  DijkstraAppSharedTest
+//
+//  Created by Chandan Sarkar on 05.04.18.
+//  Copyright © 2018 Chandan Sarkar. All rights reserved.
+//
+
+import XCTest
+
+class DijkstraAppSharedTest: XCTestCase {
+    
+    func test_XCode_Server() {
+        XCTAssertEqual("Hello World", "Hello World")
+    }
+}
---
+++ DijkstraLocationManager/DijkstraLocationManager.h
@@ -0,0 +1,19 @@
+//
+//  DijkstraLocationManager.h
+//  DijkstraLocationManager
+//
+//  Created by Chandan Sarkar on 05.04.18.
+//  Copyright © 2018 Chandan Sarkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DijkstraLocationManager.
+FOUNDATION_EXPORT double DijkstraLocationManagerVersionNumber;
+
+//! Project version string for DijkstraLocationManager.
+FOUNDATION_EXPORT const unsigned char DijkstraLocationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DijkstraLocationManager/PublicHeader.h>
+
+
